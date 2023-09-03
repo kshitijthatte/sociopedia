46f4c63e0bf0e91e7d4b6bbd1cf63469c56755c4
Kshitij Thatte <kshitijthatte@gmail.com>
2023-09-03 15:15:52 +0530

    feat: added redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from './state/';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistStore,
+  persistReducer,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = { key: 'root', storage, verson: 1 };
+const persistReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistReducer,
+  middleware: getDefaultMiddleware => {
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
